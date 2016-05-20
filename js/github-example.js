374fec48bdee4254431f6d99f00c67ca411056b1
imwr <0503wangrui@163.com>
2016-05-20 11:10:51 +0800

    init

---
+++ js/github-example.js
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+;
+$("[id^=demo]").each(function () {
+    var demo = $(this).parent();
+    demo.append($("<pre type='syntaxhighlighter' class='brush: js;html-script: true;collapse: true;class-name: paper-code;'>"
+        + demo.html() + "</pre>"));
+});
+$(document).ready(function () {
+    $("[id^=js]").each(function (i) {
+        var index = this.id.substring(2, this.id.length);
+        $("#demo" + index).parent().append($("<div class='paper-div'>&nbsp;Javascript Source</div>"
+            + "<pre type='syntaxhighlighter' class='brush: js;class-name: paper-code; toolbar: false'>"
+            + $(this).text().replace(/</g, '&lt;') + "</pre>"));
+    });
+    SyntaxHighlighter.config.strings.expandSource = "Click To Expand Html Source";
+    SyntaxHighlighter.all();
+});
