bd3f42c3b74e66773e91394b472883fc06bb3a96
王锐 <rui.wang@dmall.com>
2016-05-23 18:31:16 +0800

    代码段之后立即append

--- js/github-example.js
+++ js/github-example.js
@@ -4,13 +4,11 @@ $("[id^=demo]").each(function () {
     demo.append($("<pre type='syntaxhighlighter' class='brush: js;html-script: true;collapse: true;class-name: paper-code;'>"
         + demo.html() + "</pre>"));
 });
-$(document).ready(function () {
-    $("[id^=js]").each(function (i) {
-        var index = this.id.substring(2, this.id.length);
-        $("#demo" + index).parent().append($("<div class='paper-div'>&nbsp;Javascript Source</div>"
-            + "<pre type='syntaxhighlighter' class='brush: js;class-name: paper-code; toolbar: false'>"
-            + $(this).text().replace(/</g, '&lt;') + "</pre>"));
-    });
-    SyntaxHighlighter.config.strings.expandSource = "Click To Expand Html Source";
-    SyntaxHighlighter.all();
+$("[id^=js]").each(function (i) {
+    var index = this.id.substring(2, this.id.length);
+    $("#demo" + index).parent().append($("<div class='paper-div'>&nbsp;Javascript Source</div>"
+        + "<pre type='syntaxhighlighter' class='brush: js;class-name: paper-code; toolbar: false'>"
+        + $(this).text().replace(/</g, '&lt;') + "</pre>"));
 });
+SyntaxHighlighter.config.strings.expandSource = "Click To Expand Html Source";
+SyntaxHighlighter.all();
